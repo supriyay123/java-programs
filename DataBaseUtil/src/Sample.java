b4288cd3973712033194b5d7c34d0c7efa6c3f12
supriya <supriya@LAPTOP-636VECLC>
2022-10-20 13:05:22 +0530

    commit

---
+++ DataBaseUtil/src/Sample.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Sample {
+	public static void main(String[] args) {
+		Sample sb1 = new Sample();
+		System.out.println(sb1);
+
+		final int a = 100;
+		int[] b = { 1, 2, 3, 4 };
+		for (int i = 0; i < b.length; i++) {
+			System.out.print(b[i]+""+'\t');
+		}
+
+		
+
+	}
+
+}
